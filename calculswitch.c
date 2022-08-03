7715aeba002daec8c9501d32ba405264ee25f4ae
HIND-HANSALI <hindhsl111@gmail.com>
2022-08-03 11:20:14 +0200

    init

---
+++ calculswitch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main() {
+    int num1,num2;
+    char operateur;
+
+    printf ("Entrer le nombre1:");
+    scanf("%d",&num1);
+    printf ("Entrer l'operateur:");
+    scanf("%s",&operateur);
+    printf ("Entrer le nombre2:");
+    scanf("%d",&num2);
+    switch (operateur)
+    {
+    case '+': printf("le resultat est %d :",num1+num2);
+        break;
+    case '-': printf("le resultat est %d :",num1-num2);
+        break;
+    case '*': printf("le resultat est %d :",num1*num2);
+        break; 
+    case '/': if (num2!=0){
+    printf("le resultat est %d :",num1/num2);}
+    else {
+    printf("impossible");
+        break; } 
+    //case 'q' :        
+    
+    default:printf("cet operateur n'existe pas");
+        break;
+    }
+}
