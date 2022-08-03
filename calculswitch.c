bcb0557f3a04c1d4a7e065dea670fbba2cf2ba22
HIND-HANSALI <hindhsl111@gmail.com>
2022-08-03 12:40:19 +0200

    fff

--- calculswitch.c
+++ calculswitch.c
@@ -8,18 +8,25 @@ void main() {
     scanf("%d",&num1);
     printf ("Entrer l'operateur:");
     scanf("%s",&operateur);
+    if(operateur=='q'){
+        printf("operation arreter");
+        exit(0);
+    }
+    else{
+
     printf ("Entrer le nombre2:");
     scanf("%d",&num2);
+    }
     switch (operateur)
     {
-    case '+': printf("le resultat est %d :",num1+num2);
+    case '+': printf("le resultat est:%d",num1+num2);
         break;
-    case '-': printf("le resultat est %d :",num1-num2);
+    case '-': printf("le resultat est:%d",num1-num2);
         break;
-    case '*': printf("le resultat est %d :",num1*num2);
+    case '*': printf("le resultat est:%d",num1*num2);
         break; 
     case '/': if (num2!=0){
-    printf("le resultat est %d :",num1/num2);}
+    printf("le resultat est:%d",num1/num2);}
     else {
     printf("impossible");
         break; } 
